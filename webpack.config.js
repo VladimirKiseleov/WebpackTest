d54c11d0fc9624b839d133eb124d98766012c4db
Vladimir <kiselev7772@yandex.ru>
2021-04-23 18:20:34 +0400

    final

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TersetWebpackPlugin = require('terser-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
@@ -68,6 +69,31 @@ const jsLoaders = () => {
   return loaders
 }
 
+const plugins = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ]
+  if (isProd) {
+    base.push(new BundleAnalyzerPlugin())
+  }
+  return base
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -92,24 +118,7 @@ module.exports = {
     hot: isDev,
   },
   devtool: isDev ? 'source-map' : '',
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-      minify: {
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist'),
-      },
-    ]),
-    new MiniCssExtractPlugin({
-      filename: filename('css'),
-    }),
-  ],
+  plugins: plugins(),
   module: {
     rules: [
       {
